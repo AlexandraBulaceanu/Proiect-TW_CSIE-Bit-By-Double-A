d6cc6eadad8e764d217edceb148a34c670022340
baltatumaria19 <77074448+baltatumaria19@users.noreply.github.com>
2021-12-17 21:52:54 +0200

    Created REST functions for Companies, Locations and Routes

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ const port = process.env.PORT;
 
 //sequelize
 const db = require("./models/index.js");
-const { getReview, deleteReview } = require("./repository/Reviews.js");
-const { getUserById } = require("./repository/users.js");
+const { getReview, deleteReview, createReview, updateReview, getAllReviews } = require("./repository/Reviews.js");
+const { getUserById, deleteUser, createUser, updateUser, getAllUsers } = require("./repository/users.js");
+const { getCompany, deleteCompany, createCompany, updateCompany, getAllCompanies } = require("./repository/companies.js");
+const { getRoute, deleteRoute, createRoute, updateRoute, getAllRoutes } = require("./repository/routes.js");
+const { getLocation, deleteLocation, createLocation, updateLocation, getAllLocations } = require("./repository/locations.js");
 
 const app = express();
 
@@ -168,22 +171,265 @@ app.put("/users/:id", (req, res) => {
 	}
 });
 
-//For delete - deleteLocation
 
-// app.delete("/users/:id", (req, res) => {
-// 	try{
-// 		const id = req.params.id;
-// 		const userToDelete = deleteUser(id);
+app.delete("/users/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const userToDelete = deleteUser(id);
 
-// 		if (userToDelete.status === "success") {
-// 			res.status(200).send(`user ${id} was removed`);
-// 		} else {
-// 		res.status(404).send(`user ${id} was not found`);
-// 	}} catch(err) {
-// 		console.warn(err.stack)
-// 		res.status(500).json({ message: 'server error' })
-// 	}
-// });
+		if (userToDelete.status === "success") {
+			res.status(200).send(`user ${id} was removed`);
+		} else {
+		res.status(404).send(`user ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+});
+
+//REST for Companies
+app.post("/companies", (req, res) => {
+	try {
+	if (req.body === undefined || (Object.keys(req.body ).length === 0 && Object.getPrototypeOf(req.body ) === Object.prototype))
+    {
+      res.status(400).json({message:"body is missing"})
+    } 
+	else if(!req.body.hasOwnProperty('name'))
+    {
+      res.status(400).json({message:"malformed request"})
+    }
+	else {
+		function_response = createCompany(req.body)
+		if (function_response === null)
+			res.status(400).send()
+		else
+			res.status(200).send()
+	} } catch (err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	  }
+});
+
+app.get("/companies", (req, res) => {
+	try{
+		const companiesList = getAllCompanies();
+		res.json(companiesList);
+	} catch (err) {
+			console.warn(err.stack)
+			res.status(500).json({ message: 'server error' })
+		  }
+});
+
+
+app.get("/companies/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const company = getCompany(id);
+	
+		if (company !== null) {
+			res.json(company);
+		} else { 
+			res.status(404).send(`company ${id} was not found`);
+	} } catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+});
+
+app.put("/companies/:id", (req, res) => {
+	try{
+		let id = req.params.id;
+		let companyToUpdate = updateCompany(id); 
+		
+		if (companyToUpdate !== null) {
+		res.json(companyToUpdate);
+		} else {
+		res.status(404).send(`company ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+  });
+
+app.delete("/companies/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const companyToDelete = deleteCompany(id);
+
+		if (companyToDelete.status === "success") {
+			res.status(200).send(`company ${id} was removed`);
+		} else {
+		res.status(404).send(`company ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+  });
+
+
+  //REST for locations
+
+  app.post("/locations", (req, res) => {
+	try {
+	if (req.body === undefined || (Object.keys(req.body ).length === 0 && Object.getPrototypeOf(req.body ) === Object.prototype))
+    {
+      res.status(400).json({message:"body is missing"})
+    } 
+	else if(!req.body.hasOwnProperty('address'))
+    {
+      res.status(400).json({message:"malformed request"})
+    }
+	else {
+		function_response = createLocation(req.body)
+		if (function_response === null)
+			res.status(400).send()
+		else
+			res.status(200).send()
+	} } catch (err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	  }
+});
+
+app.get("/locations", (req, res) => {
+	try{
+		const locationsList = getAllLocations();
+		res.json(locationsList);
+	} catch (err) {
+			console.warn(err.stack)
+			res.status(500).json({ message: 'server error' })
+		  }
+});
+
+
+app.get("/locations/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const location = getLocation(id);
+	
+		if (location !== null) {
+			res.json(location);
+		} else { 
+			res.status(404).send(`location ${id} was not found`);
+	} } catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+});
+
+app.put("/locations/:id", (req, res) => {
+	try{
+		let id = req.params.id;
+		let locationToUpdate = updateLocation(id); 
+		
+		if (locationToUpdate !== null) {
+		res.json(locationToUpdate);
+		} else {
+		res.status(404).send(`location ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+  });
+
+app.delete("/locations/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const locationToDelete = deleteLocation(id);
+
+		if (locationToDelete.status === "success") {
+			res.status(200).send(`location ${id} was removed`);
+		} else {
+		res.status(404).send(`location ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+  });
+
+
+//REST for routes
+
+app.post("/routes", (req, res) => {
+	try {
+	if (req.body === undefined || (Object.keys(req.body ).length === 0 && Object.getPrototypeOf(req.body ) === Object.prototype))
+    {
+      res.status(400).json({message:"body is missing"})
+    } 
+	else if(!req.body.hasOwnProperty('wayOfTransport'))
+    {
+      res.status(400).json({message:"malformed request"})
+    }
+	else {
+		function_response = createRoute(req.body)
+		if (function_response === null)
+			res.status(400).send()
+		else
+			res.status(200).send()
+	} } catch (err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	  }
+});
+
+app.get("/routes", (req, res) => {
+	try{
+		const routesList = getAllRoutes();
+		res.json(routesList);
+	} catch (err) {
+			console.warn(err.stack)
+			res.status(500).json({ message: 'server error' })
+		  }
+});
+
+
+app.get("/routes/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const route = getRoute(id);
+	
+		if (route !== null) {
+			res.json(route);
+		} else { 
+			res.status(404).send(`route ${id} was not found`);
+	} } catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+});
+
+app.put("/routes/:id", (req, res) => {
+	try{
+		let id = req.params.id;
+		let routeToUpdate = updateRoute(id); 
+		
+		if (routeToUpdate !== null) {
+		res.json(routeToUpdate);
+		} else {
+		res.status(404).send(`route ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+  });
+
+app.delete("/routes/:id", (req, res) => {
+	try{
+		const id = req.params.id;
+		const routeToDelete = deleteRoute(id);
+
+		if (routeToDelete.status === "success") {
+			res.status(200).send(`route ${id} was removed`);
+		} else {
+		res.status(404).send(`route ${id} was not found`);
+	}} catch(err) {
+		console.warn(err.stack)
+		res.status(500).json({ message: 'server error' })
+	}
+  });
+
+
+
 
 
 app.listen(port, () => {
